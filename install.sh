8888e23f6da3ed5877570b9d8c58077ff7ca9b16
Sean <sean@madlabber.com>
2025-11-05 18:06:12 -0800

    update to installer

--- install.sh
+++ install.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
+# tools
+echo "SPCLI Installer: Installing dependencies"
+dnf install -y perl-XML-XPath mtools net-tools expect lm_sensors
+
 # files
+echo "SPCLI Installer: Copying Files"
 mkdir -p /opt/spcli/log
+if [ "$(which spcli 2>/dev/null)" != "" ];then rm "$(which spcli)";fi
 cp spcli /opt/spcli/spcli 2>/dev/null
-rm /usr/local/sbin/spcli
 ln -s /opt/spcli/spcli /usr/sbin/spcli 
 touch /opt/spcli/log/history
 touch /opt/spcli/log/system_console
 
 # permissions
+echo "SPCLI Installer: Setting permissions"
+groupadd --system libvirt 2>/dev/null
+usermod -aG libvirt $USER
 chown -R :libvirt /opt/spcli
 chmod g+rws /opt/spcli/
 chmod g+rws /opt/spcli/log
 chmod g+rw /opt/spcli/log/history
 chmod g+rw /opt/spcli/log/system_console
 
-# tools
-dnf install -y perl-XML-XPath
-dnf install -y mtools
-dnf install -y net-tools
-dnf install -y expect
-dnf install -y lm_sensors
-
-# probably not working
-usermod -aG libvirt $USER
-newgrp libvirt
+# done
+echo "SPCLI Installer: Done!"
